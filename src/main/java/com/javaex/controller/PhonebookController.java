dc0b86c20e7385d76777825dffd3f557ba3398dc
devlecture <devlecture@gmail.com>
2024-04-28 21:36:04 +0900

    exam

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -2,7 +2,6 @@ package com.javaex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +9,7 @@ import com.javaex.service.PhonebookService;
 import com.javaex.vo.PersonVo2;
 
 @RestController
-@RequestMapping("/api/mysclass")
+@RequestMapping("/api/myclass")
 public class PhonebookController {
 
 	@Autowired
