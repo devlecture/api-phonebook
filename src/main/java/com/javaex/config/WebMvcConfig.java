404c9d0479e511925415e4857fed92ecb32b7e73
devlecture <devlecture@gmail.com>
2024-04-29 13:06:28 +0900

    aaa

--- src/main/java/com/javaex/config/WebMvcConfig.java
+++ src/main/java/com/javaex/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.javaex.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
